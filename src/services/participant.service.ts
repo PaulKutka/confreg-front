52f0a68c5ce8ccb99553df7ff17440c2087be445
Vilius Lastauskas <vilius.lastauskas@euromonitor.com>
2017-03-16 11:43:15 +0200

    fixed service

--- src/services/participant.service.ts
+++ src/services/participant.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
 import { Participant } from 'models/participant';
+import { Http } from '@angular/http';
 
 @Injectable()
 export class ParticipantService {
+    private insertDataSource = "https://confregistration-api-staging.herokuapp.com/post";
+
+    constructor (private http: Http) { }
+
     insertParticipant(participant: Participant) {
-        Promise.resolve(InsertDataSource).then((participants: Participant[]) => participants.push(participant));
+        let body = JSON.stringify(participant);
+        return this.http.post(this.insertDataSource, body);
     }
 }
