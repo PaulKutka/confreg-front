f96b628dd943f5b77cb9026c8ce77d729a0ab2af
adtm <tomas.eglinskas@gmail.com>
2017-03-21 20:29:42 +0200

    Form Object Creation

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationComponent implements OnInit {
 
-  constructor() { }
+  registerForm: FormGroup;
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.registerForm = this.fb.group({
+      firstName: ['', Validators.required]
+    });
+  }
+
+  submitButtonClick(event) {
+    console.log(this.registerForm.value);
   }
 
 }
