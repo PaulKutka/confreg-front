6109c5c349d857e9cb87a00c81f0b575dda90f22
adtm <tomas.eglinskas@gmail.com>
2017-04-04 01:13:36 +0300

    radio button fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule} from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {RADIO_GROUP_DIRECTIVES} from "ng2-radio-group";
 
 import { AppComponent } from './app.component';
 import { RegistrationComponent } from './registration/registration.component';
@@ -14,7 +15,8 @@ import { RegistrationComponent } from './registration/registration.component';
   imports: [
     BrowserModule,
     ReactiveFormsModule,
-    HttpModule
+    HttpModule,
+    RADIO_GROUP_DIRECTIVES
   ],
   providers: [],
   bootstrap: [AppComponent]
